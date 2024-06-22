0cc2b87f8c18312fd11326044633292c0bd33da7
NibakureJosee <joseenibakure@gmail.com>
2024-06-23 01:22:33 +0200

    first commit

---
+++ src/test/java/rw/ne/authboilerplate/AuthBoilerplateApplicationTests.java
@@ -0,0 +1,13 @@
+package rw.ne.authboilerplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthBoilerplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
